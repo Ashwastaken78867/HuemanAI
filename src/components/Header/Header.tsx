a93b0179129600e5a84d0de6e5d8a3e51f3e2f14
Ash Bagda <abagda34@gmail.com>
2026-02-26 10:05:01 +0530

    fix asset path

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { Menu, X } from 'lucide-react';
-import logo from '/hueman.ai/src/assets/logo.png';
-
+import logo from "../../assets/logo.png";
 export function Header() {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
