c3fd40adcc2fb8b6c0edd10bee2c2e8de3953a84
Ash Bagda <abagda34@gmail.com>
2026-03-02 13:04:37 +0530

    fix animations

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -16,7 +16,7 @@ export function Header() {
   return (
     <header className="fixed top-0 left-0 right-0 z-50 bg-white">
       <div className="max-w-7xl mx-auto px-6 lg:px-8">
-        <div className="flex items-center justify-between h-15">
+        <div className="flex items-center justify-between h-20">
           {/* Logo */}
           <div className="flex items-center">
             <a href="#" className="flex items-center">
